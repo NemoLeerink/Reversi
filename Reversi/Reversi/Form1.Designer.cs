c5c622e5e7f3ad6839867ab628f25aaa7383077f
Nemo Leerink <nemo.leerink@hva.nl>
2020-12-07 17:59:47 +0100

    Versie 1 van Reversi

--- Reversi/Reversi/Form1.Designer.cs
+++ Reversi/Reversi/Form1.Designer.cs
@@ -29,13 +29,36 @@ namespace Reversi
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.panel1.Location = new System.Drawing.Point(59, 100);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 300);
+            this.panel1.TabIndex = 0;
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- Reversi/Reversi/Form1.cs
+++ Reversi/Reversi/Form1.cs
@@ -12,9 +12,118 @@ namespace Reversi
 {
     public partial class Form1 : Form
     {
+        const int breed = 6;
+        const int hoog = 6;
+        int muisX;
+        int muisY;
+        int formaatVakje;
+        bool blauwBeurt;
+
+        int[,] gameState = new int[breed, hoog];
+
         public Form1()
         {
+            int middenX = breed / 2;
+            int middenY = hoog / 2;
+
+            gameState[middenX, middenY] = 2;
+            gameState[middenX-1, middenY-1] = 2;
+            gameState[middenX, middenY-1] = 1;
+            gameState[middenX - 1, middenY] = 1;
+
+            this.blauwBeurt = true;
+
             InitializeComponent();
         }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Brush roodBrush = new SolidBrush(Color.Red);
+            Brush blauwBrush = new SolidBrush(Color.Blue);
+
+            Graphics gr = e.Graphics;
+            
+            int breedVakje = panel1.Width / breed;
+            int hoogVakje = panel1.Width / hoog;
+
+            if (breedVakje > hoogVakje)
+            {
+                breedVakje = hoogVakje;
+            }
+            else if (hoogVakje > breedVakje) 
+            {
+                hoogVakje = breedVakje;
+            }
+            formaatVakje = breedVakje;
+
+            Pen penZwart = new Pen(Color.Black, 1);
+
+
+            for (int h = 0; h <= hoog; h++)
+            {
+                gr.DrawLine(penZwart, formaatVakje * h, 0, formaatVakje * h, panel1.Height);
+            }
+
+            for (int b = 0; b <= breed; b++)
+            {
+                gr.DrawLine(penZwart, 0, formaatVakje * b, panel1.Width, formaatVakje * b);
+            }
+
+            gr.DrawLine(penZwart, panel1.Width-1, 0, panel1.Width-1, panel1.Height-1);
+            gr.DrawLine(penZwart, 0, panel1.Height-1, panel1.Width-1, panel1.Height-1);
+
+            for (int b = 0; b < breed; b++) 
+            {
+                for (int h = 0; h < hoog; h++) 
+                {
+                    if (gameState[b, h] == 1) {
+                        gr.FillEllipse(roodBrush, formaatVakje * b, formaatVakje * h, formaatVakje, formaatVakje);
+                    }
+                    else if (gameState[b, h] == 2)
+                    {
+                        gr.FillEllipse(blauwBrush, formaatVakje * b, formaatVakje * h, formaatVakje, formaatVakje);
+                    }
+                }
+            }
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            this.blauwBeurt = !this.blauwBeurt;
+            // if geen geldige beurt, doe weer this.blauwBeurt = !this.blauwBeurt;
+
+            // x / breede per vakje en y / hoogte per vakje
+            int vakjeX = this.muisX / this.formaatVakje;
+            int vakjeY = this.muisY / this.formaatVakje;
+
+            if (this.blauwBeurt)
+            {
+                this.gameState[vakjeX, vakjeY] = 2;
+            }
+            else 
+            {
+                this.gameState[vakjeX, vakjeY] = 1; 
+            }
+
+            this.Refresh();
+           
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.muisX = e.X;
+            this.muisY = e.Y;
+        }
+
+        private bool geldigeZet() 
+        {
+            bool geldigeZet;
+            
+            
+
+            return geldigeZet;
+        }
+        //methode ingesloten
+        // methode geldig
     }
 }
